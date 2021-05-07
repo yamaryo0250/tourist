fa09026f8cff00cb94cd03e10f5022c0d0fc42ba
yamaryo0250 <ryo.yamagiwa0250@gmail.com>
2021-05-07 19:25:31 +0900

    ActiveHash導入

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,9 @@
 class Ride < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :displacement
+  belongs_to :style
+  belongs_to :term
+  belongs_to :area
+
   belongs_to :user
 end
