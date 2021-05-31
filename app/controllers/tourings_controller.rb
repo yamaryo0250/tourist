56fe5d11ea25643b91f0f2b29650dcbf95eff5ef
yamaryo0250 <ryo.yamagiwa0250@gmail.com>
2021-05-31 13:12:16 +0900

    touringsテーブル、中間テーブルに保存成功

--- app/controllers/tourings_controller.rb
+++ app/controllers/tourings_controller.rb
@@ -3,4 +3,24 @@ class TouringsController < ApplicationController
     @ride = Ride.find(params[:ride_id])
     @touring = Touring.new
   end
+
+  def create
+  
+    @touring = Touring.new(touring_params)
+    if @touring.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def touring_params
+    params.require(:touring).permit(:ride_id).merge(user_ids: current_user.id)
+  end
+
+
+  private
+    
 end
